1a4eedb3f05791e76c9320b6cca851f72ae1b257
AFMT11 <AFMT11@yahoo.com>
2022-04-13 13:37:57 +0200

    1.0.0

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int Add(int a, int b)
+{
+    return a + b;
+}
+
+
+int main()
+{
+    int x, y;
+    x = 10;
+    y = 25;
+    printf("%d + %d = %d\n", x, y, Add(x, y));
+    return 0;
+}
